e8dda39d4274a79dcef2da36cc1d2a24d1893ce9
Douglas Matsumoto <douglas.matsumoto@gmail.com>
2018-07-25 11:50:05 -0300

    added scrollTo feature and small corrections on mobile layout

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // Change style of navbar on scroll
-window.onscroll = function() {myFunction()};
-function myFunction() {
+window.onscroll = function() {topNavbar()};
+function topNavbar() {
     var navbar = document.getElementById("myNavbar");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white" + " pink";
     } else {
-        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white pink", "");
     }
 }
 
@@ -17,4 +17,41 @@ function toggleFunction() {
     } else {
         x.className = x.className.replace(" w3-show", "");
     }
+}
+
+
+function scrollTo(element, to, duration) {
+    if (duration <= 0) return;
+    var difference = to - element.scrollTop;
+    var perTick = difference / duration * 10;
+
+    setTimeout(function() {
+        element.scrollTop = element.scrollTop + perTick;
+        if (element.scrollTop === to) return;
+        scrollTo(element, to, duration - 10);
+    }, 10);
+}
+
+function scrollToAbout(){
+    toggleFunction();
+    elmnt = document.getElementById("about");
+    scrollTo(document.documentElement, elmnt.offsetTop, 300);
+}
+
+function scrollToBrands(){
+    toggleFunction();
+    elmnt = document.getElementById("brands");
+    scrollTo(document.documentElement, elmnt.offsetTop, 300);
+}
+
+function scrollToContact(){
+    toggleFunction();
+    elmnt = document.getElementById("contact");
+    scrollTo(document.documentElement, elmnt.offsetTop, 300);
+}
+
+function scrollToHome(){
+    toggleFunction();
+    elmnt = document.getElementById("home");
+    scrollTo(document.documentElement, elmnt.offsetTop, 300);
 }
